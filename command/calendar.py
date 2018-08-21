0d16fbfdb5b0896929b36d87e8fb548b2ed37bc7
Carlos Alvarez <carlos.alvarez@nojazzloco.rax>
2018-08-21 11:26:25 -0500

    Adding a command/link to AARO's calendar - this is untested

---
+++ command/calendar.py
@@ -0,0 +1,18 @@
+import logging
+
+from . import MessageTypes
+from .command import Command
+
+logger = logging.getLogger(__name__)
+
+class CommandWebsite(Command):
+    """
+    AA5RObot command to create a link to the AARO calender.
+    """
+    def __init__(self):
+        self.command = "website"
+        self.syntax = None
+        self.help = "Get a link to the club's calender."
+
+    def do_command(self, data):
+        return (MessageTypes.RTM_MESSAGE, "The club website is at https://www.aa5ro.org/events")
