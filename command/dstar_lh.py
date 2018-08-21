8ef2e07f3cc2ff6c4af7c7b7558f49dada0b6923
Carlos Alvarez <carlos.alvarez@nojazzloco.rax>
2018-08-21 12:07:39 -0500

    lazy day at the office - just added some nice to haves - these are untested

---
+++ command/dstar_lh.py
@@ -0,0 +1,18 @@
+import logging
+
+from . import MessageTypes
+from .command import Command
+
+logger = logging.getLogger(__name__)
+
+class CommandWebsite(Command):
+    """
+    AA5RObot command to create a link to DStar DPlus reflector's last heard.
+    """
+    def __init__(self):
+        self.command = "dstar_lh"
+        self.syntax = None
+        self.help = "Get a link to DStar DPlus lastheard page"
+
+    def do_command(self, data):
+        return (MessageTypes.RTM_MESSAGE, "Here's a link to the DStar DPlus last heard page: http://www.dstarusers.org/lastheard.php")
